fb571d6b2103f0376be3a63df83742e1cd6d75b1
TwilightTVT <1416813305@qq.com>
2025-11-05 23:13:50 -0800

    1
    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/hy117rjj.github.com/',
+  base: '/',
   server: {
-    host: "::",
-    port: 8080,
+    host: "localhost",
+    port: 5173,
+  },
+  preview: {
+    host: "localhost",
+    port: 5173,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
